7f938cc0a1dbff785ce050e31135e1c2d3ccd9a6
Juno Nguyen <juno.ngx@gmail.com>
2023-05-19 22:26:08 +0800

    remove unused css

--- src/App.svelte
+++ src/App.svelte
@@ -15,21 +15,6 @@
 </main>
 
 <style lang="sass">
-    .logo
-        height: 6em
-        padding: 0.3rem
-        will-change: filter
-        transition: filter 300ms
-    
-    .logo:hover
-        filter: drop-shadow(0 0 2em #646cffaa)
-
-    .logo.svelte:hover
-        filter: drop-shadow(0 0 2em #ff3e00aa)
-
-    .read-the-docs
-        color: #888
-
     .content
         width: 350px
         display: flex
