0439462f63d036232a47a5d90a8c706834a87efe
Juno Nguyen <juno@pigeonlab.com>
2023-05-19 15:36:51 +0800

    Add cheek

--- src/lib/Frog.svelte
+++ src/lib/Frog.svelte
@@ -7,10 +7,19 @@
 
 <section class="frog">
     <div class="frog__eyes">
-        <div class="frog__eye frog__eye--left"/>
-        <div class="frog__eye frog__eye--right"/>
+        <div class="frog__eye frog__eye--left">
+            <div class="frog__iris frog__iris--left"/>
+        </div>
+        <div class="frog__eye frog__eye--right">
+            <div class="frog__iris frog__iris--left"/>
+        </div>
     </div>
-    <div class="frog__face"/>
+
+    <div class="frog__face">
+        <div class="frog__cheek frog__cheek--right"/>
+        <div class="frog__cheek frog__cheek--left"/>
+    </div>
+
     <div class="frog__nose">
         <div class="frog__nostril frog__eye--left"/>
         <div class="frog__nostril frog__eye--right"/>
@@ -25,17 +34,37 @@
 </section>
 
 <style lang="sass">
+
+    $bCol: LightCoral
+
     .frog
         position: relative
 
+        &__face
+
         &__eyes
 
+        &__eye
+            position: absolute
+            width: 35px
+            height: 35px
+            border-radius: 50%
+            background-color: $bCol
+
+
+
+        &__iris
+            position: absolute
+            width: 23px
+            height: 23px
+            border-radius: 50%
+            background-color: white
 
 
         &__face
             width: 180px
             height: 150px
             border-radius: 50%
-            background-color: red
+            background-color: $bCol
 
 </style>
