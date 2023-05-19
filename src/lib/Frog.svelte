f84e3bacbd9e881a83c21e16fe8bc558696fc99c
Juno Nguyen <juno@pigeonlab.com>
2023-05-19 16:00:01 +0800

    Revamp template

--- src/lib/Frog.svelte
+++ src/lib/Frog.svelte
@@ -6,31 +6,34 @@
 </script>
 
 <section class="frog">
-    <div class="frog__eyes">
-        <div class="frog__eye frog__eye--left">
-            <div class="frog__iris frog__iris--left"/>
-        </div>
-        <div class="frog__eye frog__eye--right">
-            <div class="frog__iris frog__iris--left"/>
-        </div>
-    </div>
-
     <div class="frog__face">
+
+        <!-- <div class="frog__face"> -->
         <div class="frog__cheek frog__cheek--right"/>
         <div class="frog__cheek frog__cheek--left"/>
-    </div>
+        <!-- </div> -->
 
-    <div class="frog__nose">
-        <div class="frog__nostril frog__eye--left"/>
-        <div class="frog__nostril frog__eye--right"/>
-    </div>
-
-    <div class="frog__mouth">
-        <div class="frog__lips">
-            <div class="frog__tongue">
+        <div class="frog__eyes">
+            <div class="frog__eye frog__eye--left">
+                <div class="frog__iris frog__iris--left"/>
+            </div>
+            <div class="frog__eye frog__eye--right">
+                <div class="frog__iris frog__iris--left"/>
+            </div>
         </div>
-    </div>
 
+        <div class="frog__nose">
+            <div class="frog__nostril frog__eye--left"/>
+            <div class="frog__nostril frog__eye--right"/>
+        </div>
+
+        <div class="frog__mouth">
+            <div class="frog__lips">
+                <div class="frog__tongue">
+            </div>
+        </div>
+
+    </div>
 </section>
 
 <style lang="sass">
@@ -39,32 +42,45 @@
 
     .frog
         position: relative
+        border: black 1px solid
+        width: 200px
+        height: 200px
+        display: flex
+        // place-items: center center
+        align-items: center
+        justify-content: center
 
         &__face
+            // position: absolute
+            position: relative
+            width: 160px
+            height: 120px
+            border-radius: 50%
+            background-color: blue
 
         &__eyes
+            position: relative
 
         &__eye
             position: absolute
-            width: 35px
-            height: 35px
+            width: 40px
+            height: 40px
             border-radius: 50%
-            background-color: $bCol
+            background-color: red
 
-
+            &--left
+                transform: translate(15px, -10px)
+            &--right
+                transform: translate(105px, -10px)
 
         &__iris
             position: absolute
-            width: 23px
-            height: 23px
+            width: 21px
+            height: 21px
             border-radius: 50%
             background-color: white
+            transform: translate(9px, 9px)
 
-
-        &__face
-            width: 180px
-            height: 150px
-            border-radius: 50%
-            background-color: $bCol
+        &
 
 </style>
