826b62b877474bab62161fd21a79506274cf67d4
Juno Nguyen <juno@pigeonlab.com>
2023-05-19 15:26:11 +0800

    Add frog template

---
+++ src/lib/Frog.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let count: number = 0
+    const increment = () => {
+      count += 1
+    }
+</script>
+
+<section class="frog">
+
+    <div class="frog__eyes">
+        <div class="frog__eye frog__eye--left"/>
+        <div class="frog__eye frog__eye--right"/>
+    </div>
+
+    <div class="frog__nose">
+        <div class="frog__nostril frog__eye--left"/>
+        <div class="frog__nostril frog__eye--right"/>
+    </div>
+
+    <div class="frog__mouth">
+        <div class="frog__lips">
+            <div class="frog__tongue">
+        </div>
+    </div>
+
+</section>
